85940fcec05516d9f97620783fdfc6509f80d92d
remyauda <remyauda@yahoo.fr>
2018-06-25 23:07:44 +0200

    [UPDATE] Add test in bash

--- runAll.sh
+++ runAll.sh
@@ -14,6 +14,9 @@ python main_CLI.py
 
 # Run perceptualHash Algorithms.
 cd ../../perceptualHash
+python -m perceptualHash ph1.jpg
+python -m perceptualHash --format=decimal ph1.jpg
+python -m perceptualHash --format=matrix ph1.jpg
 python perceptualHash.py ph1.jpg ph1copie.jpg
 python perceptualHash.py ph1.jpg ph2.jpg
 python perceptualHash.py ph1.jpg ph3.jpg
